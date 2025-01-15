14b027ec3efeb0acf6ed01e24868c84be4515206
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-15 21:58:37 +0000

    feat: adiciona interface `ProductRepository` para operações no banco de dados

--- src/main/java/com/stockwise/repository/ProductRepository.java
+++ src/main/java/com/stockwise/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package com.stockwise.repository;
 
 import com.stockwise.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
 }
