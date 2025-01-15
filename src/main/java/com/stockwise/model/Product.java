f89bf6f9c61eb8f38b58db272b4a8867e8111f25
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-15 21:58:14 +0000

    feat: criar entidade `Product` para mapeamento no banco de dados

--- src/main/java/com/stockwise/model/Product.java
+++ src/main/java/com/stockwise/model/Product.java
@@ -1,20 +1,50 @@
 package com.stockwise.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Table(name = "products")
-@Data // Lombok ajuda a reduzir boilerplate (getters/setters)
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private int quantity;
-    private double price;
 
-    @Column(name = "min_stock")
-    private int minStock;
+    private Integer quantity;
+
+    private Double price;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
